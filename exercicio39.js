3ef50a9a3f8d92e2f507d53d8e0cd14d7bd8a2bf
GuilhermeRCabreira <104570542+GuilhermeRCabreira@users.noreply.github.com>
2024-06-05 22:44:11 -0300

    create exercicio 38 e 39

---
+++ exercicio39.js
@@ -0,0 +1,30 @@
+/*39. Faça um algoritmo que leia um vetor (A) de 100 posições. Em seguida, compacte o
+vetor, retirando os valores nulos e negativos. Coloque o resultado no vetor B.*/
+
+const prompt = require("prompt-sync")();
+
+console.log("---- Compactação de Vetor ----");
+
+let vetorA = [];
+let vetorB = [];
+
+for (let i = 0; i < 100; i++) {
+    vetorA.push(parseFloat(input(`Digite o ${i + 1}º número: `)));
+}
+
+vetorB = vetorA.filter((el) => el > 0);
+
+console.log(`O vetor compactado é: ${vetorB}`);
+
+function input(msg) {
+    let numero;
+    do {
+        numero = prompt(msg);
+        if (isNaN(numero)) {
+            console.log("Digite um número válido.");
+        }
+        
+    } while (isNaN(numero));
+    return numero;
+}
+
